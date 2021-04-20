c915268a79203c297d5c6d697d5427e5d24e1270
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-04-20 14:23:56 -0400

    option tags

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// Create a dynamic dropdown menu from GET
+// Create a dynamic dropdown menu from GET using Await/Axios and Try/Catch //
 
 const getOptions = async () => {
   const url = 'https://covid-api.mmediagroup.fr/v1/cases?country'
   try {
     const response = await axios.get(url)
-    let countryList = Object.keys(response.data.message)
+    let countryList = Object.keys(response.data.country)
     setOptions(countryList)
     return countryList
   }
@@ -12,3 +12,19 @@ const getOptions = async () => {
     console.error(error)
   }
 }
+
+getOptions()
+
+// Create the form (dropdown menu) option tags //
+
+function setOptions(list) {
+  const selectTag = document.querySelector('#select-country')
+  list.forEach((country) => {
+    const optionTag = document.createElement('option')
+    optionTag.textContent = country
+    optionTag.value = country
+    selectTag.append(optionTag)
+  })
+  return list
+}
+
