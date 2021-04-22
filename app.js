52d7c3538516e952f148a51c7452713d4fe26789
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-04-22 01:29:24 -0400

    MVP done

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const getOptions = async () => {
   try {
     const response = await axios.get(url);
     let countryList = Object.keys(response.data);
-    // console.log(countryList);
     setOptions(countryList);
     return countryList;
   }
@@ -39,7 +38,6 @@ function getValue(e) {
   e.preventDefault();
   const optionValue = document.querySelector('#select-country').value;
   tempName = optionValue;
-  // console.log(optionValue)
   getCountryName(optionValue)
 }
 
@@ -50,72 +48,28 @@ const form = document.querySelector('form');
 form.addEventListener("submit", getValue);
 
 
-// API request for country name //
+// API request for data and create dynamic header//
 
 const getCountryName = async (getName) => {
   try {
     const nameResponse = await axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${getName}`);
-    const name  = nameResponse.data.all;
-    // appendName(name);
-    console.log(name);
+    const name = nameResponse.data.All.country;
+    document.getElementById('country-data-name').innerHTML = `${name}`;
+    const capital = nameResponse.data.All.capital_city;
+    document.getElementById('country-data-capital').innerHTML = `Capital City: ${capital}`;
+    const population = nameResponse.data.All.population;
+    document.getElementById('country-data-population').innerHTML = `Population: ${population}`;
+    const sqkm = nameResponse.data.All.sq_km_area;
+    document.getElementById('country-data-sqkm').innerHTML = `Sq-Km: ${sqkm}`;
+    const confirmed = nameResponse.data.All.confirmed;
+    document.getElementById('country-data-confirmed').innerHTML = `Confirmed: ${confirmed}`;
+    const recovered = nameResponse.data.All.recovered;
+    document.getElementById('country-data-recovered').innerHTML = `Recovered: ${recovered}`;
+    const deaths = nameResponse.data.All.deaths;
+    document.getElementById('country-data-deaths').innerHTML = `Capital City: ${deaths}`;
     return name;
   }
   catch (error) {
     console.error(error);
   }
-}
-
-// Create dynamic header tag for country name //
-
-// function appendName(getName) {
-//   const nameText = document.createTextNode(`Country: ${getName}`);
-//   getName.appendChild(nameText);
-//   document.getElementById("country-data-name").appendChild(getName);
-// }
-
-
-// API request attempt 2 //
-
-// function showCountryName(name) {
-//   let nameResponse = axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${getValue}`);
-//   document.getElementById('country-data-name').innerHTML = `Country: ${nameResponse.data}`;
-//   return nameResponse;
-// }
-
-// API request for population data //
-
-async function getPopulationData(populationValue) {
-  try {
-    const populationResponse = await axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${getValue}`);
-    const population = populationResponse.data.all.population;
-    console.log(population);
-    appendPopulation(population);
-    return population;
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
-
-
-// API request for confirmed cases //
-
-async function getConfirmedCases(confirmedValue) {
-  try {
-    const confirmedResponse = await axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${getValue}`)
-    const confirmed = confirmedResponse.data.all.confirmed;
-    console.log(confirmed);
-    appendConfirmed(confirmed);
-    return confirmed;
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
-
-
-// Remove previous text div //
-
-function removeText() {
-  const removeTextDiv = document.querySelector()
 }
